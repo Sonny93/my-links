600874d135bc992d340acb9b34c45cd561e7c4a9
Sonny <sonnyasdev@gmail.com>
2023-11-16 12:17:53 +0100

    feat(wip): convert category button to link

--- src/components/SideMenu/Categories/Categories.tsx
+++ src/components/SideMenu/Categories/Categories.tsx
@@ -1,24 +1,36 @@
 import { useTranslation } from "next-i18next";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { Category } from "types";
-import CategoryItem from "./CategoryItem";
 import styles from "./categories.module.scss";
+import { motion } from "framer-motion";
+import CategoryItem from "./CategoryItem";
 
 interface CategoriesProps {
   categories: Category[];
   categoryActive: Category;
   handleSelectCategory: (category: Category) => void;
 }
+
 export default function Categories({
   categories,
   categoryActive,
-  handleSelectCategory,
+  handleSelectCategory
 }: CategoriesProps) {
   const { t } = useTranslation();
+  const itemsRef = useRef<Array<HTMLLIElement>>([]);
   const linksCount = useMemo(
     () => categories.reduce((acc, current) => (acc += current.links.length), 0),
     [categories]
   );
+
+  console.log("render");
+  useEffect(() => {
+    itemsRef.current.forEach((ref) => {
+      if (ref.id === categoryActive.id.toString()) {
+        ref.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    });
+  }, [categoryActive.id]);
 
   return (
     <div className={styles["categories"]}>
@@ -27,13 +39,26 @@ export default function Categories({
       </h4>
       <ul className={styles["items"]}>
         {categories.map((category, index) => (
-          <CategoryItem
-            category={category}
-            categoryActive={categoryActive}
-            handleSelectCategory={handleSelectCategory}
+          <motion.li
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 25,
+              delay: index * 0.02,
+              duration: 200
+            }}
+            style={{
+              transition: "none"
+            }}
+            ref={el => itemsRef.current[index] = el}
+            title={category.name}
             key={category.id}
-            index={index}
-          />
+            id={category.id.toString()}
+          >
+            <CategoryItem category={category} categoryActive={categoryActive} />
+          </motion.li>
         ))}
       </ul>
     </div>
--- src/components/SideMenu/Categories/CategoryItem.tsx
+++ src/components/SideMenu/Categories/CategoryItem.tsx
@@ -1,56 +1,23 @@
-import { motion } from "framer-motion";
-import { useEffect, useRef } from "react";
 import { AiFillFolderOpen, AiOutlineFolder } from "react-icons/ai";
 import { Category } from "types";
 import styles from "./categories.module.scss";
+import LinkTag from "next/link";
 
 interface CategoryItemProps {
   category: Category;
   categoryActive: Category;
-  handleSelectCategory: (category: Category) => void;
-  index: number;
 }
 
 export default function CategoryItem({
   category,
-  categoryActive,
-  handleSelectCategory,
-  index,
+  categoryActive
 }: CategoryItemProps): JSX.Element {
-  const ref = useRef<HTMLLIElement>();
   const className = `${styles["item"]} ${
     category.id === categoryActive.id ? styles["active"] : ""
   }`;
-  const onClick = () => handleSelectCategory(category);
-
-  useEffect(() => {
-    if (category.id === categoryActive.id) {
-      ref.current.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
-  }, [category.id, categoryActive.id]);
 
   return (
-    <motion.li
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 25,
-        delay: index * 0.02,
-        duration: 200,
-      }}
-      className={className}
-      ref={ref}
-      onClick={onClick}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: ".25em",
-        transition: "none",
-      }}
-      title={category.name}
-    >
+    <LinkTag href={`/?categoryId=${category.id}`} className={className}>
       {category.id === categoryActive.id ? (
         <AiFillFolderOpen size={24} />
       ) : (
@@ -61,6 +28,6 @@ export default function CategoryItem({
         <span className={styles["name"]}>{category.name}</span>
         <span className={styles["links-count"]}>— {category.links.length}</span>
       </div>
-    </motion.li>
+    </LinkTag>
   );
 }
